40af46235a3f8425599dfe6daa0836ea2868b3d1
Vaisakh Manu <133499771+ItzMeHyper@users.noreply.github.com>
2025-01-07 21:58:05 +0530

    Add files via upload

---
+++ Details.sh
@@ -0,0 +1,18 @@
+echo Enter your Name:
+read name
+echo Enter your Semester:
+read semester
+echo Enter your Department:
+read dept
+echo Enter your Batch:
+read batch
+echo Enter your Roll Number:
+read roll
+echo Enter your register number:
+read reg
+echo Name: $name
+echo Semester: $semester
+echo Department: $dept
+echo Batch: $batch
+echo Roll Number: $roll
+echo Register Number: $reg
